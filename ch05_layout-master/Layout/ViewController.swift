c3791dee6a6e93d1be80dc5a38a44e85d7e587a1
acarriono <44483888+acarriono@users.noreply.github.com>
2019-03-25 22:11:14 -0500

    Add files via upload

---
+++ ch05_layout-master/Layout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Layout
+//
+//  Created by Andrea Carrion on 2/20/19.
+//  Copyright © 2019 AndreaCarrion. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ ch05_restructure-master/Restructure/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Restructure
+//
+//  Created by Andrea Carrion on 3/21/19.
+//  Copyright © 2019 AndreaCarrion. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
